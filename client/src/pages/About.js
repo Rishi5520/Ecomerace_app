981cd257c6cc1bb3bc494c930860083882e0f781
Rishiraj Shinde <Rishirajshinde1610@gmail.com>
2024-01-14 08:57:08 +0530

    all order statys

---
+++ client/src/pages/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+
+const About = () => {
+  return (
+    <Layout title={"About us - Ecommer app"}>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/about.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p className="text-justify mt-2">
+            About your company and teach
+          </p>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default About;
