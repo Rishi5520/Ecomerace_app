e2010ea5b4a5de38c65311557ec855e4ac840d7d
Rishiraj shinde <rishirajshinde1610@gmail.com>
2024-01-29 11:39:39 +0530

    update server js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 //configure env
 dotenv.config();
@@ -22,6 +23,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname,'./client/build')))
 
 //routes
